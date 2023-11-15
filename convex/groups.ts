10dca19105ea1168670e1eddf1a43aab3509e7ab
Graehm <gpatrellfazio@gmail.com>
2023-11-14 19:19:08 -0600

    msg group functionality

--- convex/groups.ts
+++ convex/groups.ts
@@ -7,3 +7,27 @@ export const get = query({
     return await ctx.db.query('groups').collect();
   },
 });
+
+// export const get = query({
+// 	args: {},
+// 	handler: async (ctx) => {
+// 		return await ctx.db.query('groups').collect();
+// 	}
+// });
+
+// export const getGroup = query({
+// 	args: { id: v.id('groups') },
+// 	handler: async (ctx, { id }) => {
+// 		return await ctx.db
+// 			.query('groups')
+// 			.filter((q) => q.eq(q.field('_id'), id))
+// 			.unique();
+// 	}
+// });
+
+// export const create = mutation({
+// 	args: { description: v.string(), name: v.string(), icon_url: v.string() },
+// 	handler: async ({ db }, args) => {
+// 		await db.insert('groups', args);
+// 	}
+// });
