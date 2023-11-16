136f3e80e76f21aca758a01f2785cfc6ca8f9353
Graehm <gpatrellfazio@gmail.com>
2023-11-15 20:40:43 -0600

    msg group backend in convex

--- convex/groups.ts
+++ convex/groups.ts
@@ -1,5 +1,5 @@
-import { query } from './_generated/server';
-
+import { mutation, query } from './_generated/server';
+import { v } from 'convex/values'
 // This is a query that returns all the groups in the database
 export const get = query({
   args: {},
@@ -8,26 +8,19 @@ export const get = query({
   },
 });
 
-// export const get = query({
-// 	args: {},
-// 	handler: async (ctx) => {
-// 		return await ctx.db.query('groups').collect();
-// 	}
-// });
+export const getGroup = query({
+  args: { id: v.id('groups') },
+  handler: async (ctx, { id }) => {
+    return await ctx.db.
+      query('groups')
+      .filter((q) => q.eq(q.field('_id'), id))
+      .unique()
+  }
+})
 
-// export const getGroup = query({
-// 	args: { id: v.id('groups') },
-// 	handler: async (ctx, { id }) => {
-// 		return await ctx.db
-// 			.query('groups')
-// 			.filter((q) => q.eq(q.field('_id'), id))
-// 			.unique();
-// 	}
-// });
-
-// export const create = mutation({
-// 	args: { description: v.string(), name: v.string(), icon_url: v.string() },
-// 	handler: async ({ db }, args) => {
-// 		await db.insert('groups', args);
-// 	}
-// });
+export const create = mutation({
+  args: {description: v.string(), name: v.string(), icon_url: v.string()},
+  handler: async ({db}, args) => {
+    await db.insert('groups', args)
+  }
+})
