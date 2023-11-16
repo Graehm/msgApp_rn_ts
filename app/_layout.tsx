7ecb16164464e474c7340044e7eaccbeb24044fa
Graehm <gpatrellfazio@gmail.com>
2023-11-15 21:52:05 -0600

    modaling in the house

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import { Ionicons } from '@expo/vector-icons'
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
   unsavedChangesWarning: false,
 })
-
+// essentially the app-server functionality
 export default function RootLayoutNav() {
   return (
     <ConvexProvider client={convex}>
@@ -18,6 +18,7 @@ export default function RootLayoutNav() {
         },
         headerTintColor: '#fff',
       }}>
+        {/* sets up create chat modal icon */}
         <Stack.Screen 
           name="index"
           options={{
@@ -31,6 +32,21 @@ export default function RootLayoutNav() {
             )
           }}
         />
+        {/* create modal routing here for when a user clicks the create a new chat icon */}
+          <Stack.Screen 
+            name="(modal)/create"
+            options={{
+              headerTitle: 'Start a Chat',
+              presentation: 'modal',
+              headerLeft: () => (
+                <Link href={'./'} asChild>
+                  <TouchableOpacity>
+                    <Ionicons name="close-outline" size={32} color="White" />
+                  </TouchableOpacity>
+                </Link>
+              )
+            }}
+          />
       </Stack>
     </ConvexProvider>
   );
