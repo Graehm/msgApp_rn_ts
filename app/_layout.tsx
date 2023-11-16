5902e63afb6306e2965d95e4d2d153181be977ff
Graehm <gpatrellfazio@gmail.com>
2023-11-16 13:37:01 -0600

    re wrote layout tsx and modal is working at the moment

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,54 +1,53 @@
-import { Link, Stack } from 'expo-router'
-import { ConvexProvider, ConvexReactClient } from 'convex/react'
-import React from 'react'
+import { Link, Stack } from 'expo-router';
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import React from 'react';
 import { TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons';
 
 const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
-  unsavedChangesWarning: false,
-})
-// essentially the app-server functionality
+	unsavedChangesWarning: false
+});
+
 export default function RootLayoutNav() {
-  return (
-    <ConvexProvider client={convex}>
-      <Stack 
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#EEA217',
-        },
-        headerTintColor: '#fff',
-      }}>
-        {/* sets up create chat modal icon */}
-        <Stack.Screen 
-          name="index"
-          options={{
-            headerTitle: 'My Chats',
-            headerRight: () => (
-              <Link href={'/(modal)/create'} asChild>
-                <TouchableOpacity>
-                  <Ionicons name="add" size={32} color="white" />
-                </TouchableOpacity>
-              </Link>
-            )
-          }}
-        />
-        {/* create modal routing here for when a user clicks the create a new chat icon. it grabs the path of create new chat form line 38 */}
-          {/* <Stack.Screen 
-            name="(modal)/create"
-            options={{
-              headerTitle: 'Start a Chat',
-              presentation: 'modal',
-              headerLeft: () => (
-                <Link href={'/'} asChild>
-                  <TouchableOpacity>
-                    <Ionicons name="close-outline" size={32} color="white" />
-                  </TouchableOpacity>
-                </Link>
-              )
-            }}
-          /> */}
-          {/* <Stack.Screen name="(chat)/[chatid]" options={{ headerTitle: 'Test' }} /> */}
-      </Stack>
-    </ConvexProvider>
-  );
+	return (
+		<ConvexProvider client={convex}>
+			<Stack
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: '#EEA217'
+					},
+					headerTintColor: '#fff'
+				}}
+			>
+				<Stack.Screen
+					name="index"
+					options={{
+						headerTitle: 'My Chats',
+						headerRight: () => (
+							<Link href={'/(modal)/create'} asChild>
+								<TouchableOpacity>
+									<Ionicons name="add" size={32} color="white" />
+								</TouchableOpacity>
+							</Link>
+						)
+					}}
+				/>
+				<Stack.Screen
+					name="(modal)/create"
+					options={{
+						headerTitle: 'Start a Chat',
+						presentation: 'modal',
+						headerLeft: () => (
+							<Link href={'/'} asChild>
+								<TouchableOpacity>
+									<Ionicons name="close-outline" size={32} color="white" />
+								</TouchableOpacity>
+							</Link>
+						)
+					}}
+				/>
+				<Stack.Screen name="(chat)/[chatid]" options={{ headerTitle: '' }} />
+			</Stack>
+		</ConvexProvider>
+	);
 }
