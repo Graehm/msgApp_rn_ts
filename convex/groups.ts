81662a6ca99a8b7b4738d44f4998c72e9e939a29
Graehm <gpatrellfazio@gmail.com>
2023-11-27 13:10:36 -0600

    debugging

--- convex/groups.ts
+++ convex/groups.ts
@@ -1,6 +1,5 @@
+import { v } from 'convex/values';
 import { mutation, query } from './_generated/server';
-import { v } from 'convex/values'
-
 
 // This is a query that returns all the groups in the database
 export const get = query({
@@ -10,21 +9,21 @@ export const get = query({
   },
 });
 
-// fetches groups and their data 
+// This is a query that returns a single group by ID
 export const getGroup = query({
   args: { id: v.id('groups') },
   handler: async (ctx, { id }) => {
-    return await ctx.db.
-      query('groups')
+    return await ctx.db
+      .query('groups')
       .filter((q) => q.eq(q.field('_id'), id))
-      .unique()
-  }
-})
+      .unique();
+  },
+});
 
-// creates new group
+// This is a mutation that creates a new group
 export const create = mutation({
-  args: {description: v.string(), name: v.string(), icon_url: v.string()},
-  handler: async ({db}, args) => {
-    await db.insert('groups', args)
-  }
-})
+  args: { description: v.string(), name: v.string(), icon_url: v.string() },
+  handler: async ({ db }, args) => {
+    await db.insert('groups', args);
+  },
+});
