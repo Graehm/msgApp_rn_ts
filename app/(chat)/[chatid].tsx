1ebedfe967a60a4b8f4fa68b3c49563fd91d5e05
Graehm <gpatrellfazio@gmail.com>
2023-11-27 14:18:17 -0600

    continueing to build out hoping that ill discover something under the hoot thats the problem

--- app/(chat)/[chatid].tsx
+++ app/(chat)/[chatid].tsx
@@ -4,6 +4,7 @@ import { useLocalSearchParams } from 'expo-router'
 
 const Page = () => {
     const { id } = useLocalSearchParams()
+    // console.log('~file: [chatid].tsx:7 ~ Page ~ id:', id)
     return (
         <View>
             <Text>If you see this then it is working</Text>
