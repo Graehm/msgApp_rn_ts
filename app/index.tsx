df248a1a849720fdf6e2f9e474cd7b3822b527db
Graehm <gpatrellfazio@gmail.com>
2023-11-30 12:45:18 -0600

    added page layout styling and previous bugs are absent however I am having an issue with a property issue and no props

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useQuery } from 'convex/react'
 import { api } from '../convex/_generated/api'
@@ -13,8 +13,14 @@ const Page = () => {
         <View style={{flex: 1}}>
             <ScrollView style={styles.container}>
                 {groups.map((group) => (
-                    <Link href={{ pathname: '/(chat)/[chatid]', params: { chatid: group._id } }} key={group._id.toString()}>
-                        <Text>{group.name}</Text>
+                    <Link href={{ pathname: '/(chat)/[chatid]', params: { chatid: group._id } }} key={group._id.toString()} asChild>
+                        <TouchableOpacity style={styles.group}>
+                            <Image source={{ uri: group.icon_url }} style={{ width: 50, height: 50 }} />
+                            <View style={{ flex: 1 }}>
+                                <Text>{group.name}</Text>
+                                <Text style={{ color: '#888' }}>{group.name}</Text>
+                            </View>
+                        </TouchableOpacity>
                     </Link>
                 ))}
             </ScrollView>
@@ -28,6 +34,22 @@ const styles = StyleSheet.create({
         padding: 10, 
         backgroundColor: '#F8F5EA'
     },
+    group: {
+        flexDirection: 'row',
+        gap: 10, 
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 10, 
+        margin: 10, 
+        shadowColor: '#000',
+        elevation: {
+            width: 0, 
+            height: 1,
+        }, 
+        shadowOpacity: 0.22, 
+        shadowRadius: 2.22
+    }
 })
 
 
