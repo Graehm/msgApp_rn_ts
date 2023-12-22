c96ac1c22d8c081e509cd400230f2242355e7867
Graehm <gpatrellfazio@gmail.com>
2023-12-22 17:34:40 -0600

    building out new group chat page with no auth but instead user info

--- app/(modal)/create.tsx
+++ app/(modal)/create.tsx
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, KeyboardAvoidingView, Platform, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { useRouter } from 'expo-router'
 
 const Page = () => {
+    const [name, setName] = useState('')
+    const [desc, setDesc] = useState('')
+    const [icon, setIcon] = useState('')
+    const startGroup = useMutation(api.groups.create)
+    const router = useRouter()
+//create a new group in the convex mutation 
+    const onCreateGroup = async () => {
+        await startGroup({
+            name, 
+            description: desc, 
+            icon_url: icon
+        })
+        router.back()
+    }
     return (
-        <View>
+        <KeyboardAvoidingView>
             <Text>Yo!</Text>
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 
