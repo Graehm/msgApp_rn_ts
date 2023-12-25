6514ba45272b8aac931d413a22aabc662458af02
Graehm <gpatrellfazio@gmail.com>
2023-12-25 12:16:05 -0600

    create page complete but modal not working

--- app/(modal)/create.tsx
+++ app/(modal)/create.tsx
@@ -1,59 +1,72 @@
-import { View, Text, KeyboardAvoidingView, Platform, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { useMutation } from 'convex/react'
-import { api } from '@/convex/_generated/api'
-import { useRouter } from 'expo-router'
+import { Text, KeyboardAvoidingView, Platform, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useRouter } from 'expo-router';
 
 const Page = () => {
-    const [name, setName] = useState('')
-    const [desc, setDesc] = useState('')
-    const [icon, setIcon] = useState('')
-    const router = useRouter()
-    const startGroup = useMutation(api.groups.create)
-//create a new group in the convex mutation 
-    const onCreateGroup = async () => {
-        await startGroup({
-            name, 
-            description: desc, 
-            icon_url: icon
-        })
-        router.back()
+  const [name, setName] = useState('');
+  const [desc, setDesc] = useState('');
+  const [icon, setIcon] = useState('');
+  const startGroup = useMutation(api.groups.create);
+  const router = useRouter();
 
+  // Create a new group with Convex mutation
+  const onCreateGroup = async () => {
+    await startGroup({
+      name,
+      description: desc,
+      icon_url: icon,
+    });
+    router.back();
+  };
 
-    }
-    return (
-        <KeyboardAvoidingView style={styles.container}>
-            <Text style={styles.label}>Name</Text>
-            <TextInput style={styles.textInput} value={name} onChangeText={setName}></TextInput>
-           
-            <Text style={styles.label}>Desccription</Text>
-            <TextInput style={styles.textInput} value={desc} onChangeText={setDesc}></TextInput>
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'} keyboardVerticalOffset={100}>
+      <Text style={styles.label}>Name</Text>
+      <TextInput style={styles.textInput} value={name} onChangeText={setName} />
+      <Text style={styles.label}>Description</Text>
+      <TextInput style={styles.textInput} value={desc} onChangeText={setDesc} />
+      <Text style={styles.label}>Icon URL</Text>
+      <TextInput style={styles.textInput} value={icon} onChangeText={setIcon} />
+      <TouchableOpacity style={styles.button} onPress={onCreateGroup}>
+        <Text style={styles.buttonText}>Create</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+};
 
-            <Text style={styles.label}>Icon URL</Text>
-            <TextInput style={styles.textInput} value={icon} onChangeText={setIcon}></TextInput>
-        </KeyboardAvoidingView>
-    )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F8F5EA',
+    padding: 10,
+  },
+  label: {
+    marginVertical: 10,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    minHeight: 40,
+    backgroundColor: '#fff',
+  },
+  button: {
+    backgroundColor: '#EEA217',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
-const styles = StyleSheet.create ({
-    container: {
-        flex:1, 
-        backgroundColor: '#F8F5EA',
-        padding: 10, 
-    },
-    label: {
-        marginVertical: 10,
-    },
-    textInput: {
-        borderWidth: 1, 
-        borderColor: 'gray',
-        borderRadius: 5,
-        paddingHorizontal: 10, 
-        minHeight: 40, 
-        backgroundColor: '#fff'
-    }
-})
-export default Page
-
-
-
+export default Page;
